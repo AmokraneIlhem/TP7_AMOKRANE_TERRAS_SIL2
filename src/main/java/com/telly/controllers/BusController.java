542b474f73c22fcb59d8a1c3a39105873698498e
TERRAS Juba <ij_terras@esi.dz>
2022-01-20 10:33:34 +0100

    Affichage des réservations

--- src/main/java/com/telly/controllers/BusController.java
+++ src/main/java/com/telly/controllers/BusController.java
@@ -18,49 +18,66 @@ import com.telly.service.BusService;
 @Controller
 public class BusController {
 
-	@Autowired
-	BusService busService;
-	
-
-	@RequestMapping("/createtrip")
-	public String reserveBus(Model model, Principal principal) {
-
-		model.addAttribute("bus", new Bus());
-
-		return "createtrip";
-	}
-
-	@RequestMapping(value = "/createreserve", method = RequestMethod.POST)
-	public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
-			Principal principal) {
-
-		if (result.hasErrors()) {
-			return "reservebus";
-		}
-
-		busService.create(bus);
-
-		return "home";
-	}
-	@RequestMapping("/results")
-	public String leave(Model model, Principal principal) {
-
-		model.addAttribute("bus", new Bus());
-
-		return "results";
-	}
+    @Autowired
+    BusService busService;
 
 
-	@RequestMapping(value = "/resultsfrom", method = RequestMethod.GET)
-	public String leaveFrom(@Validated(FormValidationGroup.class) Bus bus, BindingResult result, Model model,
-							Principal principal) {
+    @RequestMapping("/createtrip")
+    public String reserveBus(Model model, Principal principal) {
 
-		List<Bus> results = busService.getCity(bus.getLeaveFrom(), bus.getGoingTo(), bus.getDateLeave(),
-				bus.getDateReturn());
-		model.addAttribute("results", results);
-		System.out.println(results);
+        model.addAttribute("bus", new Bus());
 
-		return "results";
+        return "createtrip";
+    }
 
-	}
+    @RequestMapping(value = "/createreserve", method = RequestMethod.POST)
+    public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
+                                Principal principal) {
+
+        if (result.hasErrors()) {
+            return "reservebus";
+        }
+
+        busService.create(bus);
+
+        return "home";
+    }
+
+    @RequestMapping("/results")
+    public String leave(Model model, Principal principal) {
+
+        model.addAttribute("bus", new Bus());
+
+        return "results";
+    }
+
+
+    @RequestMapping(value = "/resultsfrom", method = RequestMethod.GET)
+    public String leaveFrom(@Validated(FormValidationGroup.class) Bus bus, BindingResult result, Model model,
+                            Principal principal) {
+
+        List<Bus> results = busService.getCity(bus.getLeaveFrom(), bus.getGoingTo(), bus.getDateLeave(),
+                bus.getDateReturn());
+        model.addAttribute("results", results);
+        System.out.println(results);
+
+        return "results";
+
+    }
+
+    @RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+    public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+        String username = principal.getName();
+        reserve.getUser().setUsername(username);
+
+        List<Reserve> reserves = reserveService.getReserves(username);
+        model.addAttribute("reserves", reserves);
+        System.out.println(reserves);
+
+
+        return "home";
+
+    }
 }
